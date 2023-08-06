7371d8aae89c6531e7bb982b23de4ad165154b0f
Ivan <techydad05@gmail.com>
2023-08-06 14:49:52 -0400

    trying to fix..

---
+++ src/routes/cart/+page.js
@@ -0,0 +1,19 @@
+import Medusa from "@medusajs/medusa-js";
+
+export async function load() {
+    let baseUrl = "http://192.168.1.42:9002";
+
+    const medusa = new Medusa({ baseUrl: baseUrl, maxRetries: 3 });
+
+    let usRegionId = "";
+
+    let regions = await medusa.regions.list();
+    regions.regions.forEach((ele) => {
+        ele.countries.forEach((val) => {
+            if (val.iso_2 == "us") {
+                usRegionId = ele.id;
+            }
+        });
+    });
+    return { usRegionId: usRegionId, medusa: medusa };
+}
